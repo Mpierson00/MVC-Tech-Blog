ab4e1d5deb97cb306f494b2449d92a4c6770eafa
Michael <mpierson00@gmail.com>
2024-05-21 23:51:15 -0400

    updated home route to include post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -57,5 +57,37 @@ router.get('/dashboard', async (req, res) => {
     res.status(500).json(err);
   }
 });
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        },
+        {
+          model: Comment,
+          include: [{ model: User, attributes: ['username'] }]
+        }
+      ]
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    const post = postData.get({ plain: true });
+
+    res.render('post-details', {
+      ...post,
+      loggedIn: req.session.loggedIn
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 
 module.exports = router;
