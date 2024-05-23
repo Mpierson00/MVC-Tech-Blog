1543a2d7a1b962652d93a4b3eb616a21dbf65006
Michael <mpierson00@gmail.com>
2024-05-23 19:08:20 -0400

    added ability for password to be empty

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,12 @@ require('dotenv').config();
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_PASSWORD || '',
   {
     host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
-    port: process.env.DB_PORT || 3306
+    port: process.env.DB_PORT || 3306,
+    logging: console.log,
   }
 );
 
